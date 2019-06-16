a8b23bb603427b81f1466e2ee76011c48ae17319
Wayne Wallace <51827303+apvmedia@users.noreply.github.com>
2019-06-16 08:30:49 -0700

    added remove

--- shopping_list_2.py
+++ shopping_list_2.py
@@ -12,6 +12,7 @@ def show_help():
 		Enter 'DONE' to stop adding items. 
 		Enter 'HELP' for help.
 		Enter 'SHOW' to show list.
+		Enter 'REMOVE' to remove from list.
 	""")
 
 def add_to_list(item):
@@ -45,16 +46,30 @@ def show_list():
 
 	print("-" * 10)
 
+def remove_from_list():
+	show_list()
+	what_to_remove = input("What would you like to remove?\n> ")
+	try:
+		shopping_list.remove(what_to_remove)
+	except ValueError:
+		pass
+	show_list()
+
 show_help()
+
 while True:
-	new_item = raw_input(">> ")
-	if new_item.upper == 'DONE' or new_item.upper == 'QUIT':
+	new_item = input(">> ")
+
+	if new_item.upper() == 'DONE' or new_item.upper() == 'QUIT':
 		break
-	elif new_item.upper == 'HELP':
+	elif new_item.upper() == 'HELP':
 		show_help()
 		continue
-	elif new_item.upper == 'SHOW':
+	elif new_item.upper() == 'SHOW':
 		show_list()
+		continue
+	elif new_item.upper() == 'REMOVE':
+		remove_from_list()
 		continue
 	else:
 		add_to_list(new_item)
