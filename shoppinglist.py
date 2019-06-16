98340a581804a5c3dc45aa11e1bffe60b3ea210f
apvmedia <51827303+apvmedia@users.noreply.github.com>
2019-06-15 19:31:43 -0700

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -2,14 +2,22 @@
 
 shopping_list = []
 
-def add_to_list(item):
-	shopping_list.append(item)
-
 def show_help():
 	print("What should we pick up at the store?")
-	print("""Enter 'DONE' to stop adding items. Enter 'HELP' for help.""")
+	print("""
+		Enter 'DONE' to stop adding items. 
+		Enter 'HELP' for help.
+		Enter 'SHOW' to show list.
+	""")
 
-new_item = " "
+def add_to_list(item):
+	shopping_list.append(item)
+	print("Added: List has {} items".format(len(shopping_list)))
+
+def show_list():
+	print("Here's your list:")
+	for item in shopping_list:
+		print(item)
 
 show_help()
 while True:
@@ -19,5 +27,11 @@ while True:
 	elif new_item == 'HELP':
 		show_help()
 		continue
+	elif new_item == 'SHOW':
+		show_list()
+		continue
 
 	add_to_list(new_item)
+
+
+show_list()
