1ec476697794cac93957c225b131440ebac1309a
Wayne Wallace <51827303+apvmedia@users.noreply.github.com>
2019-06-15 19:24:09 -0700

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -7,19 +7,17 @@ def add_to_list(item):
 
 def show_help():
 	print("What should we pick up at the store?")
-	print(""")
-Enter 'DONE' to stop adding items.
-Enter 'HELP' for help.
-""")
+	print("""Enter 'DONE' to stop adding items. Enter 'HELP' for help.""")
+
+new_item = " "
 
 show_help()
 while True:
-	new_item = input("> ")
-
+	new_item = raw_input(">> ")
 	if new_item == 'DONE':
 		break
 	elif new_item == 'HELP':
 		show_help()
 		continue
 
-	add_to_list(item)
+	add_to_list(new_item)
